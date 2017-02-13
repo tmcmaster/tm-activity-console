67b4736f2b2d42f3f2042dc6f3ff440a15ffb66a
Tim McMaster <tim.mcmaster@iselect.com.au>
2017-02-14 10:33:58 +1100

    init

---
+++ src/main/java/au/id/mcmaster/activityconsole/ActivityConsoleMain.java
@@ -0,0 +1,12 @@
+package au.id.mcmaster.activityconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityConsoleMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityConsoleMain.class, args);
+	}
+}
